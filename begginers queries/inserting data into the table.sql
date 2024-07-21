7f4219eeff6a8f3fb06fd74bf682b8e7b84b473c
Faraz Ahmad <farazahmad31048@gmail.com>
2024-07-22 02:46:38 +0500

    inserting data

--- begginers queries/inserting data into the table.sql
+++ begginers queries/inserting data into the table.sql
@@ -1,5 +1,14 @@
 -- Inserting data into the table;
 
+--INSERT INTO persons(id, name, city)
+--VALUES (101, 'Faraz Ahmad','Lahore'); -- to insert single entry
+
+-- Multiple Entries;
+
+INSERT INTO persons(id, name, city) VALUES
+(102, 'Rana Mubashir','Lake City'),
+(103,'Zubair Khan','OPS'),
+(104,'Rehan Rasheed','Chungi Amersadu');
 
 
 
